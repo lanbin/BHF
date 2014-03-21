f81411d26f7eb6c81be13998e311b81bbff67cd1
lanbin <lanbin1987@gmail.com>
2014-03-21 18:00:01 +0800

    update

--- js/directive/issue-list.source.js
+++ js/directive/issue-list.source.js
@@ -9,12 +9,13 @@ angular.module("BHF")
             link: function($scope, $element, $attrs){
                $scope.openComment = function($event){
                     var ct = $event.currentTarget,
-                        cb = $(ct).siblings(".comment-box");
+                        cb = $(ct).parent().siblings(".comment-box");
 
-                    if($(cb).attr("display") != "block") {
+                    if(!$(cb).is(":visible")) {
+                        $element.find(".comment-box").hide();  
                         $(cb).show();
                     }else{
-                        $element.find(".comment-box").hide();    
+                        $element.find(".comment-box").hide();  
                     }
                }
             }
