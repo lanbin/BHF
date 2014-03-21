4db55b01a5db20481296ab71d4838fde9c56edff
lanbin <lanbin1987@gmail.com>
2014-03-21 15:09:19 +0800

    add alert

--- js/controller/index.source.js
+++ js/controller/index.source.js
@@ -5,27 +5,30 @@ angular.module('BHF')
 		$rootScope.pageName = "首页"
 
 		//监听添加成功事件，参数为事件对象，和数据
-		$rootScope.$on("addProjectSuccess", function(event, data) {
-			console.log("refresh project wall", data)
+		$rootScope.$on("addProjectSuccess", function(event) {
+			getAllProject(true);
 		});
 
-		API.getAllProjects(true).then(function(data) {
-			$scope.projectSorts = [{
-				name: "进行中的项目",
-				sortkey: "w",
-				items: data.processing
-			}, {
-				name: "未开始的项目",
-				sortkey: "d",
-				items: data.fresh
-			}, {
-				name: "已完成的项目",
-				sortkey: "q",
-				items: data.done
-			}, {
-				name: "被取消的项目",
-				sortkey: "d",
-				items: data.rejected
-			}]
-		});
+		function getAllProject(freshData) {
+			API.getAllProjects(true).then(function(data) {
+				$scope.projectSorts = [{
+					name: "进行中的项目",
+					sortkey: "w",
+					items: data.processing
+				}, {
+					name: "未开始的项目",
+					sortkey: "d",
+					items: data.fresh
+				}, {
+					name: "已完成的项目",
+					sortkey: "q",
+					items: data.done
+				}, {
+					name: "被取消的项目",
+					sortkey: "d",
+					items: data.rejected
+				}]
+			});
+		}
+		getAllProject(true);
 	})
--- js/directive/addproject-dialog.source.js
+++ js/directive/addproject-dialog.source.js
@@ -4,10 +4,10 @@ angular.module("BHF")
 	.directive("apd", function($rootScope, API) {
 		var dpOpt = {
 			format: 'yyyy-mm-dd HH:ii', //时间格式
-			autoclose: true,			//点击后立刻关闭
-			language: "zh-CN",			//语言包
-			minView: "0",				//时间最小粒度
-			weekStart: 1 				//一周从星期一开始
+			autoclose: true, //点击后立刻关闭
+			language: "zh-CN", //语言包
+			minView: "0", //时间最小粒度
+			weekStart: 1 //一周从星期一开始
 		};
 
 		return {
@@ -16,14 +16,15 @@ angular.module("BHF")
 			replace: true,
 			link: function($scope, $element, $attrs) {
 
-				var start = $("#project-start-date"),
-					end = $("#project-end-date"),
+				var start = $("#p-start-date"),
+					end = $("#p-end-date"),
 					dialog = $('#addProjectDialog'),
 					addBtn = $("#addBtn"),
 					posting = false;
 
 				//配置日期选择器
 				start.datetimepicker(dpOpt);
+				start.datetimepicker("setStartDate", new Date());
 				end.datetimepicker(dpOpt);
 
 				//开始时间设置后，设置结束时间的起始时间
@@ -33,29 +34,52 @@ angular.module("BHF")
 
 				//关闭弹窗后所有的内容清空
 				dialog.on('hidden.bs.modal', function(e) {
-					$(this).find('input').val("");
+					$(this).find('input').val("").removeAttr("disabled");
 				})
 
 				//提交新项目
 				addBtn.on("click", function(e) {
-					dialog.find('input').attr("disabled", "disabled");
+					if (posting) return;
+					posting = true;
+
 					var formData = {
 						title: $("#p-name").val(),
 						description: $("#p-description").val(),
-						contact:$("#p-contact").val(),
+						contact: $("#p-contact").val(),
 						start_date: $("#p-start-date").val(),
 						end_date: $("#p-end-date").val(),
-						repos:$("#p-repos").val(),
+						repos: $("#p-repos").val(),
 						creator: 'someuser',
 						timestamp: (new Date()).getTime()
 					}
 
+					if (formData.title == "" ||
+						formData.description == "" ||
+						formData.contact == "" ||
+						formData.start_date == "" ||
+						formData.end_date == "") {
+						Messenger().error({
+							message: "所有标星项均需要填写！",
+							id: "post"
+						});
+						posting = false;
+						return;
+					}
+
+					dialog.find('input').attr("disabled", "disabled");
 					API.addProject(formData).then(function(data) {
-						console.log(data)
-						$rootScope.$emit("addProjectSuccess");
+						if (data.data.id) {
+							$rootScope.$emit("addProjectSuccess");
+							dialog.modal('hide');
+							Messenger().post({
+								message: "新项目已增加，请前往项目中填写需求！",
+								showCloseButton: true,
+								type: "success",
+								id: "post"
+							});
+							posting = false;
+						}
 					});
-					
-					
 				});
 			}
 		}
